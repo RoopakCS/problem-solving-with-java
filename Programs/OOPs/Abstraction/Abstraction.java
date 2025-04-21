ae5ed52641900591c1157ca7f91cad7c38991935
RoopakCS <roopaksenthilkumar@gmail.com>
2025-04-21 11:19:33 +0530

    Added Programs

--- Programs/OOPs/Abstraction/Abstraction.java
+++ Programs/OOPs/Abstraction/Abstraction.java
@@ -25,6 +25,40 @@ class Car extends Vehicle {
 
 }
 
+interface location {
+    public void cleanLocation();
+
+    public void hourlyMessage();
+}
+
+interface defaultUser {
+    public void showIdCard();
+
+    public void hourlyMessage();
+}
+
+class serviceManager implements location, defaultUser {
+
+    @Override
+    public void showIdCard() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'showIdCard'");
+    }
+
+    @Override
+    public void cleanLocation() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cleanLocation'");
+    }
+
+    @Override
+    public void hourlyMessage() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hourlyMessage'");
+    }
+
+}
+
 public class Abstraction {
     public static void main(String[] args) {
         Car car = new Car();
