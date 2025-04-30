75339faad77b6f0b47bf0bb3db1b66475ed92a26
RoopakCS <roopaksenthilkumar@gmail.com>
2025-04-30 10:37:03 +0530

    Added Backtracking

--- Programs/Recursion/GCD/GcdOfANumber.java
+++ Programs/Recursion/GCD/GcdOfANumber.java
@@ -1,13 +1,24 @@
 public class GcdOfANumber {
-    public static int gcd(int a, int b) {
-        if (a == 0) {
-            return b;
-        }
-        return gcd(b % a, a);
-    }
+    // public static int gcd(int a, int b) {
+    // if (a == 0) {
+    // return b;
+    // }
+    // return gcd(b % a, a);
+    // }
+
+    // public static void main(String[] args) {
+    // int a = 35, b = 15;
+    // System.out.printf("The GCD of %d and %d is: %d", a, b, gcd(a, b));
+    // }
 
     public static void main(String[] args) {
-        int a = 35, b = 15;
-        System.out.printf("The GCD of %d and %d is: %d", a, b, gcd(a, b));
+        System.out.println(hcf(35, 49));
+    }
+
+    public static int hcf(int a, int b) {
+        if (b == 0)
+            return a;
+
+        return hcf(b, a % b);
     }
 }
--- Programs/Recursion/Reverse A String/ReverseString.java
+++ Programs/Recursion/Reverse A String/ReverseString.java
@@ -1,16 +1,29 @@
 public class ReverseString {
-    public static String reverse(String str, String result, int length) {
-        if (length < 0) {
-            return result;
-        }
-        result = result + str.charAt(length);
-        return reverse(str, result, length - 1);
-    }
+    // public static String reverse(String str, String result, int length) {
+    // if (length < 0) {
+    // return result;
+    // }
+    // result = result + str.charAt(length);
+    // return reverse(str, result, length - 1);
+    // }
+
+    // public static void main(String[] args) {
+    // String str = "Recursion";
+    // String result = "";
+    // int length = str.length() - 1;
+    // System.out.println(reverse(str, result, length));
+    // }
 
     public static void main(String[] args) {
-        String str = "Recursion";
-        String result = "";
-        int length = str.length() - 1;
-        System.out.println(reverse(str, result, length));
+        String input = "four";
+        System.out.println(revStr(input));
+
+    }
+
+    public static String revStr(String input) {
+        if (input.isEmpty())
+            return input;
+
+        return revStr(input.substring(1)) + input.charAt(0);
     }
 }
